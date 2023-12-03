377be655f245469125c9ffa2f30f628e7291c406
2pac <tarasyaremabcn@gmail.com>
2023-12-03 18:07:05 +0100

    chore: day 3 ez

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs;
 
@@ -25,6 +25,7 @@ fn main() {
     // Logic goes here
 
     let mut nums: Vec<([usize; 3], i32)> = Vec::new();
+    let mut gears: HashMap<[usize; 2], Vec<i32>> = HashMap::new();
 
     for (i, line) in contents.trim().lines().enumerate() {
         let mut maybe_num = "".to_owned();
@@ -42,15 +43,25 @@ fn main() {
                         maybe_num = "".to_owned();
                     }
 
+                    if c == '*' {
+                        gears.insert([i + 1, j + 1], Vec::new());
+                    }
+
                     if c != '.' {
                         nums.push(([i + 1, j + 1, 1], -1));
                     }
                 }
             }
         }
+
+        // Edge case when the number ends in the end
+        if maybe_num.len() > 0 {
+            let num = maybe_num.parse::<i32>().expect("could not parse number");
+            nums.push(([i + 1, line.len(), maybe_num.len()], num));
+        }
     }
 
-    let mut num_set: HashSet<i32> = HashSet::new();
+    let mut num_set: HashSet<(usize, usize, i32)> = HashSet::new();
 
     for (i, ([x1, x2, xl], xn)) in nums.iter().enumerate() {
         for (j, ([y1, y2, yl], yn)) in nums.iter().enumerate() {
@@ -58,9 +69,12 @@ fn main() {
                 continue;
             }
 
-            if *yn == -1 {
+            let mut found = false;
+
+            if *yn == -1 && *xn != -1 {
                 for k in 0..*xl {
                     let dist = distance([*x1, *x2 - k], [*y1, *y2]);
+
                     // println!(
                     //     "{} & {} -> dist([{}, {}], [{}, {}]) = {}",
                     //     *xn,
@@ -73,12 +87,19 @@ fn main() {
                     // );
 
                     if dist {
-                        num_set.insert(*xn);
+                        found = true;
+                        num_set.insert((*x1, *x2, *xn));
+                    }
+                }
+
+                if found {
+                    if let Some(v) = gears.get_mut(&[*y1, *y2]) {
+                        v.push(*xn)
                     }
                 }
             }
 
-            if *xn == -1 {
+            if *xn == -1 && *yn != -1 {
                 for k in 0..*yl {
                     let dist = distance([*y1, *y2 - k], [*x1, *x2]);
                     // println!(
@@ -93,14 +114,28 @@ fn main() {
                     // );
 
                     if dist {
-                        num_set.insert(*yn);
+                        num_set.insert((*y1, *y2, *yn));
+                    }
+                }
+
+                if found {
+                    if let Some(v) = gears.get_mut(&[*x1, *x2]) {
+                        v.push(*yn)
                     }
                 }
             }
         }
     }
 
-    let res1: i32 = num_set.into_iter().sum();
+    let res1: i32 = num_set.into_iter().map(|(_, _, x)| x).sum();
+    let res2: i32 = gears.values().fold(0, |acc, v| {
+        if v.len() == 2 {
+            acc + v.iter().fold(1, |v_acc, x| v_acc * x)
+        } else {
+            acc
+        }
+    });
 
     println!("1. {res1}");
+    println!("2. {res2}");
 }
