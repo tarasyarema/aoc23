e27fcecd1e16961b996f34908e26e0a5c57273cb
2pac <tarasyaremabcn@gmail.com>
2023-12-10 23:08:18 +0100

    chore: ffs

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -3,17 +3,6 @@ use std::env;
 use std::fs;
 use std::time::Instant;
 
-pub fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            std::mem::swap(&mut m, &mut n);
-        }
-        m %= n;
-    }
-    n
-}
-
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     let mut file = "in".into();
@@ -99,13 +88,10 @@ fn main() {
 
     println!("1. {res1} in {:?}", start.elapsed());
 
-    let mut res2 = 0;
+    let mut res2: u64 = 0;
 
     let start2 = Instant::now();
     let r_len = roots2.len();
-
-    let og_roots2 = roots2.clone();
-    let mut cycle: HashMap<&str, u64> = HashMap::new();
 
     loop {
         let mut end = false;
@@ -115,10 +101,8 @@ fn main() {
             res2 += 1;
 
             roots2 = roots2
-                .clone()
                 .into_iter()
-                .enumerate()
-                .map(|(i, root)| {
+                .map(|root| {
                     let new_root = if s {
                         // left
                         graph.get(root).unwrap().0
@@ -128,15 +112,6 @@ fn main() {
                     };
 
                     if new_root.ends_with("Z") {
-                        if !cycle.contains_key(new_root) {
-                            cycle.insert(new_root, res2);
-
-                            println!(
-                                "Found for root {i} at {} > {new_root} in {res2} steps",
-                                og_roots2[i]
-                            );
-                        }
-
                         ok += 1;
                     }
 
@@ -144,44 +119,24 @@ fn main() {
                 })
                 .collect();
 
-            if cycle.len() == r_len {
+            if res2 % 100_000_000 == 0 {
+                println!(
+                    "{} M iterations in elpased {:?}",
+                    (res2 as f64) / 1_000_000.0,
+                    start2.elapsed()
+                );
+            }
+
+            if ok == r_len {
                 end = true;
                 break;
             }
         }
 
-        // println!("{res2} {:?}", roots2);
-
         if end {
             break;
         }
     }
-
-    let vals: Vec<u64> = cycle.clone().values().into_iter().cloned().collect();
-    let mut c_gcd = 1;
-
-    loop {
-        let new_gcd = cycle.values().fold(vals[0], |acc, &x| gcd(acc, x / c_gcd));
-
-        if new_gcd == 1 {
-            break;
-        }
-
-        c_gcd = c_gcd * new_gcd;
-    }
-
-    res2 = cycle.values().fold(1, |acc, x| {
-        println!(
-            "{} {} {} {} {}",
-            acc,
-            x,
-            c_gcd,
-            x / c_gcd,
-            acc * (x / c_gcd)
-        );
-
-        acc * (x / c_gcd)
-    });
 
     println!("2. {} in {:?}", res2, start2.elapsed());
 }
